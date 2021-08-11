e19e0ab5ae9f8941b9bfc8477bd7da1d2a818146
Callum Scott <callumdscott@protonmail.com>
2021-08-11 16:09:09 +0100

    Updated documentation of Singly Linked List Node

--- Structures/LinkedLists/Node.py
+++ Structures/LinkedLists/Node.py
@@ -1,4 +1,6 @@
 class Node:
+    """Node for storing data of Singly Linked Lists."""
     def __init__(self, object_to_hold):
+        """Initialises a Node for Singly Linked Lists."""
         self.object_held = object_to_hold
         self.next = None
