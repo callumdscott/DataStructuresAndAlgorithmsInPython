c6e10c19939c623e04c2482f91b2528aebea0e20
Callum Scott <callumdscott@protonmail.com>
2021-08-27 13:59:08 +0100

    completed basic binary search class

--- Alogrithms/BinarySearch.py
+++ Alogrithms/BinarySearch.py
@@ -1,49 +1,44 @@
 class BinarySearch:
     """Binary Search class implementation for Lists."""
 
-    def __init__(self, low=0, high=0, dataset=None):
+    def __init__(self, dataset):
         """Creates a binary search object."""
-        self.low = low
-        self.high = high
+        self.low = 0
+        self.high = len(dataset) - 1
         self.mid = self.low + ((self.high - self.low) // 2)
         self.dataset = dataset
 
-    def find_nearest_val(self, val):
-        """Finds Value closest to the target val"""
-        pass
+    def find_nearest_val(self, val) -> [int, int]:
+        """Finds value and index tuple closest to the target val"""
+        # self.mid will point to the index position where to insert the val, ie. val larger than index val
+        while self.low <= self.high:
+            if self.dataset[self.mid] == val:
+                return val, self.mid
+            elif self.dataset[self.mid] > val:
+                self.high = self.mid - 1
+            else:
+                self.low = self.mid + 1
+            self.mid = self.low + ((self.high - self.low) // 2)
+        # if mid is the last element we have found the closest value
+        if self.mid == len(self.dataset) - 1:
+            return self.dataset[-1], len(self.dataset) - 1
+        # find the differences between the target and the values either side of it's place in the list
+        difference_at_mid = abs(self.dataset[self.mid] - val)
+        difference_next = abs(self.dataset[self.mid + 1] - val)
+        # test if the previous or the current position has the closest value to our target
+        if difference_at_mid <= difference_next:
+            return self.dataset[self.mid], self.mid
+        else:
+            return self.dataset[self.mid + 1], self.mid + 1
 
     def find_val(self, val):
         """Returns True if value is present"""
-        pass
-
-    def find_first(self, val):
-        """Returns index of the first occurrence of target val"""
-        pass
-
-    def find_last(self, val):
-        """Returns index of the last occurrence of target val"""
-        pass
-
-    def find_all(self, val):
-        """Returns index of the first and last occurrence of target val"""
-        pass
-
-    def find_insert_pos(self, val):
-        """Returns the index position to insert a target val"""
-        pass
-
-    def insert(self, val):
-        """Inserts the target val in the list, maintaining order"""
-        pass
-
-    def insert_n_times(self, val, n):
-        """Inserts the target val in the list n times, maintaining order"""
-        pass
-
-    def any_within_variance(self, val, variance):
-        """Returns a list of values in the target range, +- variance"""
-        pass
-
-    def find_values_in_range(self, low, high):
-        """Returns a list of values in the range low - high"""
-        pass
+        while self.low <= self.high:
+            if self.dataset[self.mid] == val:
+                return True
+            elif self.dataset[self.mid] > val:
+                self.high = self.mid - 1
+            else:
+                self.low = self.mid + 1
+            self.mid = self.low + ((self.high - self.low) // 2)
+        return False
